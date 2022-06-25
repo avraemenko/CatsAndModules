0ca823717c4f0050b838913d8f3da911cbdc1ce3
Kateryna Avramenko <kavramenko2000@gmail.com>
2022-06-25 14:02:49 +0200

    Initial Commit

---
+++ CatsAndModules_KaterynaAvramenko/CatsAndModules_KaterynaAvramenkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAndModules_KaterynaAvramenkoApp.swift
+//  CatsAndModules_KaterynaAvramenko
+//
+//  Created by Kate Avramenko on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatsAndModules_KaterynaAvramenkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
